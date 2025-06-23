1a2fb0270b458b24738d260b823bbc17c7c46f08
sewlesew <sewlesewbiazen65@gmail.com>
2025-06-23 08:05:19 +0300

    feature: port adjustment

--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -5,20 +5,9 @@ const userController = require("../../controllers/user.controller");
 
 const userRoutes = express.Router();
 
-
 // Routes...
 userRoutes.post("/register", userController.registerUser);
 userRoutes.get("/getAll", userController.getUsers);
 userRoutes.get("/user/:id", userController.getUser);
-
-// userRoutes.post("/login", login);
-
-// userRoutes.post("/forgotpw", forgotPassword);
-// userRoutes.post("/resetpw", resetPassword);
-
-// userRoutes.use(auth);
-
-// // Protected routes...
-// userRoutes.get("/dashboard", userDashboard);
 
 module.exports = userRoutes;
--- server.cjs
+++ server.cjs
@@ -1,14 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const connectDB = require('./utils/connectDB')
 
-// port the app listen to
-const port = process.env.PORT || 3000
 dotenv.config()
+
+// port the app listen to
+const port = process.env.PORT || 8000
 
 // app
 const app = express()
@@ -22,11 +22,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+// static files
+app.use(express.static('public'))
 
 // routes
 const routes = require('./routes')
 app.use('/api', routes)
-
 
 // error handling middleware
 app.use((err, req, res, next) => {  
@@ -35,5 +36,5 @@ app.use((err, req, res, next) => {
 
 const hostname = process.env.HOSTNAME || 'localhost'
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
